5f70fde27b199fa75b923fc1099223eb95eb5602
Mahmoud Nabhan <niypoo@gmail.com>
2023-11-04 13:34:24 +0200

    remove locale

--- lib/locales/ar.dart
+++
@@ -1,6 +0,0 @@
-const Map<String, String> qrCodeScannerModuleLocalAr = {
-  "Simply point the camera and scan the code.":
-      "ببساطة قم بتوجيه الكاميرا ومسح الرمز ضوئيًا.",
-  "Point camera at the code": "وجّه الكاميرا إلى الرمز",
-  "Code scanner": "الماسح الضوئي للرمز",
-};
--- lib/locales/en.dart
+++
@@ -1,6 +0,0 @@
-const Map<String, String> qrCodeScannerModuleLocalEn = {
-  "Simply point the camera and scan the code.":
-      "Simply point the camera and scan the code.",
-  "Point camera at the code": "Point camera at the code",
-    "Code scanner": "Code scanner",
-};
